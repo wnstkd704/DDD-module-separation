bacc9f419767734a772aa752ad1154079d44efbd
junsang <wnstkd704@gmail.com>
2026-09-19 21:28:06 +0900

    chore: Spring Boot 프로젝트 초기 설정

---
+++ src/main/java/com/example/dddmoduleseparation/DddModuleSeparationApplication.java
@@ -0,0 +1,13 @@
+package com.example.dddmoduleseparation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddModuleSeparationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DddModuleSeparationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dddmoduleseparation/DddModuleSeparationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dddmoduleseparation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddModuleSeparationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
